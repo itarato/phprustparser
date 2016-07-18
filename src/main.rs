0947a5947b23e9f00039100d4de00cc6d46ab12f
Peter Arato <it.arato@gmail.com>
2016-07-19 01:33:58 +0800

    Parse start token.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 fn main() {
     let source = load_source();
-    let mut reader = Reader::new(source);
+    let mut tokenizer = Tokenizer::new(source);
 
-    reader.addTokenReader(Box::new(|| {
-        println!("one tr");
-        TokenReaderResult
+    tokenizer.addTokenReader(Box::new(|reader, states| {
+        if states.phpFileState == PhpFileState::Outside {
+            if reader.peek_char_n(5) == "<?php" {
+                states.phpFileState = PhpFileState::Inside;
+                reader.forward_n(5);
+                return Some(Token::PhpStart);
+            }
+        }
+        None
     }));
 
-    reader.run();
+    tokenizer.addTokenReader(Box::new(|reader, states| {
+        reader.forward();
+        Some(Token::Whitespace)
+    }));
+
+    tokenizer.run();
 }
 
 fn load_source() -> String {
@@ -17,10 +28,9 @@ echo($foo);
 ".to_string();
 }
 
-struct TokenReaderResult;
+type TokenReader = Fn(&mut Reader, &mut ReaderStateCollection) -> Option<Token>;
 
-type TokenReader = Fn() -> TokenReaderResult;
-
+#[derive(Debug)]
 enum Token {
     PhpStart,
     VariableName(String),
@@ -30,15 +40,8 @@ enum Token {
     FunctionName(String),
     ParenthesisOpen,
     ParenthesisClose,
+    Whitespace,
 }
-
-#[derive(PartialEq)]
-enum ReaderState {
-    Read,
-    Finish,
-}
-
-impl Default for ReaderState { fn default() -> Self { ReaderState::Read } }
 
 #[derive(PartialEq)]
 enum PhpFileState {
@@ -49,14 +52,15 @@ enum PhpFileState {
 impl Default for PhpFileState { fn default() -> Self { PhpFileState::Outside } }
 
 #[derive(Default)]
+struct ReaderStateCollection {
+    phpFileState: PhpFileState,
+}
+
+#[derive(Default)]
 struct Reader {
     source: String,
     chars: Vec<char>,
     position: usize,
-    tokens: Vec<Token>,
-    readerState: ReaderState,
-    phpFileState: PhpFileState,
-    tokenReaders: Vec<Box<TokenReader>>,
 }
 
 impl Reader {
@@ -69,25 +73,63 @@ impl Reader {
         }
     }
 
+    fn peek_char(&self) -> char {
+        self.chars[self.position]
+    }
+
+    fn peek_char_n(&self, n: usize) -> String {
+        self.chars.iter().skip(self.position).take(n).map(|ch| *ch).collect()
+    }
+
+    fn forward(&mut self) {
+        self.position += 1
+    }
+
+    fn forward_n(&mut self, n: usize) {
+        self.position += n
+    }
+
+    fn is_end(&self) -> bool {
+        self.position >= self.chars.len()
+    }
+
+}
+
+struct Tokenizer {
+    reader: Reader,
+    tokens: Vec<Token>,
+    states: ReaderStateCollection,
+    tokenReaders: Vec<Box<TokenReader>>,
+}
+
+impl Tokenizer {
+    fn new(source: String) -> Tokenizer {
+        Tokenizer {
+            reader: Reader::new(source),
+            tokens: Default::default(),
+            states: Default::default(),
+            tokenReaders: Default::default(),
+        }
+    }
+
     fn addTokenReader(&mut self, tr: Box<TokenReader>) { self.tokenReaders.push(tr); }
 
     fn run(&mut self) {
-        while self.readerState == ReaderState::Read {
+        while !self.reader.is_end() {
             self.read();
         }
     }
 
     fn read(&mut self) {
-        let c = self.chars[self.position];
-        println!("{:#?}", c);
-
         for tr in &self.tokenReaders {
-            tr();
-        }
-
-        self.position += 1;
-        if self.position >= self.chars.len() {
-            self.readerState = ReaderState::Finish;
+            match tr(&mut self.reader, &mut self.states) {
+                 Some(token) => {
+                    println!("Token: {:#?}", token);
+                    self.tokens.push(token);
+                    break;
+                 },
+                 None => { },
+             }
         }
     }
 }
